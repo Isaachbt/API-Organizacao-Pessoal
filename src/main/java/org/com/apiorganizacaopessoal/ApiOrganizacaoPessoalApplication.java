67d9af78929900360a02028179dd2f8dba90a10f
Isaachbt <isaac.silva1478@gmail.com>
2025-08-04 20:24:40 -0300

    Initial commit

---
+++ src/main/java/org/com/apiorganizacaopessoal/ApiOrganizacaoPessoalApplication.java
@@ -0,0 +1,13 @@
+package org.com.apiorganizacaopessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrganizacaoPessoalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiOrganizacaoPessoalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/com/apiorganizacaopessoal/ApiOrganizacaoPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.apiorganizacaopessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrganizacaoPessoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
